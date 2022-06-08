c83d5228fdd9908932b6e09efba347bab6aa80ec
Nauja <jeremymorosi@hotmail.fr>
2022-06-08 18:11:43 +0200

    update version

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export { IPlayer } from 'moroboxai-player-sdk';
 /**
  * Version of the player.
  */
-export const VERSION: string = '0.1.0-alpha.5';
+export const VERSION: string = '0.1.0-alpha.6';
 
 
 const sdkConfig: MoroboxAIPlayerSDK.ISDKConfig = {
