60aaf6b8942b86f828f63977b3cde9faa7c2bef0
Nauja <jeremymorosi@hotmail.fr>
2023-08-27 09:26:22 +0200

    bump

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export { IPlayer } from 'moroboxai-player-sdk';
 /**
  * Version of the player.
  */
-export const VERSION: string = '0.1.0-alpha.15';
+export const VERSION: string = '0.1.0-alpha.17';
 
 
 const sdkConfig: MoroboxAIPlayerSDK.ISDKConfig = {
