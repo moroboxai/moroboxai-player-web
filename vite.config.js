43f581a4ac1dcec393f37af0c90539623af264b2
Nauja <jeremymorosi@hotmail.fr>
2023-09-16 08:41:24 +0200

    improve build

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,5 @@
 import { defineConfig } from "vite";
-const { resolve } = require("path");
 
 export default defineConfig({
-    build: {
-        outDir: resolve(__dirname, "lib"),
-        lib: {
-            entry: resolve(__dirname, "src", "index.ts"),
-            name: "MoroboxAIPlayer",
-            formats: ["cjs", "umd"],
-            fileName: (format) => {
-                switch (format) {
-                    case "cjs":
-                        return `cjs/index.cjs`;
-                    case "umd":
-                        return `umd/moroboxai-player-web.min.js`;
-                }
-            }
-        },
-        rollupOptions: {
-            input: {
-                main: "./index.html"
-            }
-        }
-    },
     base: "/moroboxai-player-web/"
 });
