53c7fcefa67cd88e3e11bce01e2381b3f726e0fb
Nauja <jeremymorosi@hotmail.fr>
2023-09-19 08:50:10 +0200

    bump

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ export { VERSION as PLAYER_SDK_VERSION } from "moroboxai-player-sdk";
 /**
  * Version of the player.
  */
-export const VERSION: string = "0.1.0-alpha.33";
+export const VERSION: string = "0.1.0-alpha.34";
 
 const sdkConfig: MoroboxAIPlayerSDK.ISDKConfig = {
     inputController: () => new InputController(),
