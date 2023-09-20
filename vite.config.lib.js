6913708d88abd7a7f3aeb4a253b493aa0dd093d4
Nauja <jeremymorosi@hotmail.fr>
2023-09-20 18:26:36 +0200

    inject version

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -1,17 +1,23 @@
 import { defineConfig } from "vite";
+import {version} from "./package.json";
 const { resolve } = require("path");
 
 export default defineConfig({
+    define: {
+        "__VERSION__": version
+    },
     build: {
         outDir: resolve(__dirname, "lib"),
         lib: {
             entry: resolve(__dirname, "src", "index.ts"),
             name: "MoroboxAIPlayer",
-            formats: ["cjs", "umd"],
+            formats: ["cjs", "es", "umd"],
             fileName: (format) => {
                 switch (format) {
                     case "cjs":
                         return `cjs/index.cjs`;
+                    case "es":
+                        return `es/index.js`;
                     case "umd":
                         return `umd/moroboxai-player-web.min.js`;
                 }
